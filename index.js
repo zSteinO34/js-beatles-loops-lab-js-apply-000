29cf668a9944561c464987a2e8ce422186a9a118
Zac Steinberg <zstein1234@gmail.com>
2019-07-31 20:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function theBeatlesPlay(musicians, instruments) {
 theBeatlesPlay(musicians, instruments);
 
 
-var i = 0;
 var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
+var i = 0;
 
 function johnLennonFacts(facts) {
   var newArr = [];
