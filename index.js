8bc0eb1d2ca5fc90d55f4b22c5e9c1ad5a72616d
Zac Steinberg <zstein1234@gmail.com>
2019-07-31 20:19:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function theBeatlesPlay(musicians, instruments) {
   var result = [];
   for(var i = 0; i < musicians.length; i++) {
      var str = `${musicians[i]} plays ${instruments[i]}`;
-     return result.push(str);
+     result.push(str);
+     return result;
   }
 }
 
