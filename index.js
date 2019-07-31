619260338553b6f553a3a31c28e2950c263fbc96
Zac Steinberg <zstein1234@gmail.com>
2019-07-31 20:13:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+var musicians = [
+  "John Lennon",
+  "Stevie Wonder",
+  "Louie Armstrong"
+  ];
+var instruments = [
+  "guitar",
+  "piano",
+  "saxaphone"
+  ];
+
+function theBeatlesPlay(musicians, instruments) {
+  var result = [];
+  for(var i = 0; i < musicians.length; i++) {
+     var str = `${musicians[i]} plays ${instruments[i]}`;
+     result.push(str);
+  }
+}
